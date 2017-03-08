41db0a2259aa2d872c3510426482142fce3c674c
iYgnohZ <iygnohz@gmail.com>
2017-03-08 23:13:07 +0800

    README.md

--- industry_and_commerce.py
+++ industry_and_commerce.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
+
 from geetest import BaseGeetestCrack
 from selenium import webdriver
 
 
-class ExpNormalGeetestCrack(BaseGeetestCrack):
+class IndustryAndCommerceGeetestCrack(BaseGeetestCrack):
 
     """工商滑动验证码破解类"""
 
     def __init__(self, driver):
-        super(ExpNormalGeetestCrack, self).__init__(driver)
+        super(IndustryAndCommerceGeetestCrack, self).__init__(driver)
 
     def crack(self):
         """执行破解程序
-
         """
-        self.move_to_element()
+        self.input_by_id()
+        self.click_by_id()
+        time.sleep(2)
         x_offset = self.calculate_slider_offset()
         self.drag_and_drop(x_offset=x_offset)
 
 
 def main():
     driver = webdriver.Chrome()
-    driver.get("http://www.geetest.com/exp_normal")
-    cracker = ExpNormalGeetestCrack(driver)
+    driver.get("http://bj.gsxt.gov.cn/sydq/loginSydqAction!sydq.dhtml")
+    cracker = IndustryAndCommerceGeetestCrack(driver)
     cracker.crack()
+    print(driver.get_window_size())
+    time.sleep(3)
+    driver.save_screenshot("screen.png")
+    driver.close()
 
 
 if __name__ == "__main__":
